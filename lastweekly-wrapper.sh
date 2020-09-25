c6534aeb80cc3d9bba6e3f6ef3fad786d9ba802c
Kevin Spencer <kevin@kevinspencer.org>
2020-09-25 14:03:44 -0700

    altered calling params

--- lastweekly-wrapper.sh
+++ lastweekly-wrapper.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-LFM_API_KEY=$(<.lastfm.token); export LFM_API_KEY
+LFM_API_KEY=$(<lastfm.token); export LFM_API_KEY
+LFM_USER_ID=$(<lastfm.user); export LFM_USER_ID
 
-./lastweekly.pl --user kevinspencer --twitter
+./lastweekly.pl --user ${LFM_USER_ID} --twitter
